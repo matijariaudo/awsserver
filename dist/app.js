83357e805b08c3463426cfaa81df7936bc3c6789
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2023-08-29 19:31:04 +1000

    4

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -30,5 +39,11 @@ client.on('qr', (qr) => {
 client.on('ready', () => {
     console.log('Client is ready!');
 });
-client.on('message', (m) => { console.log(m); });
-client.initialize();
+client.on('message', (m) => { console.log(m); })(() => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.initialize();
+    }
+    catch (error) {
+        console.error('Error during initialization:', error);
+    }
+}))();
